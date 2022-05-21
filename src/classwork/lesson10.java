d3783cca46a755a38718ed4b994703546ea471c5
MushVardanyan96 <mister.musho@mail.ru>
2022-05-21 22:05:12 +0400

    ArrayUtil

---
+++ src/classwork/lesson10.java
@@ -0,0 +1,25 @@
+package classwork;
+
+public class lesson10 {
+    public static void main(String[] args) {
+        int month = 7;
+
+        String season;
+        if (month == 12 || month == 1 || month == 2) {
+            season = " dzmer";
+
+        } else if (month == 3 || month == 4 || month == 5) {
+            season = " garun";
+        } else if (month == 6 || month == 7 || month == 8) {
+            season = " amar";
+        } else if (month == 9 || month == 10 || month == 11) {
+            season = "ashun";
+        } else
+            season = "nman amis goyutyun chuni";
+        System.out.println();
+
+
+    }
+
+
+}
--- src/homework/arrayutil/CharArrayExample.java
+++ src/homework/arrayutil/CharArrayExample.java
@@ -22,7 +22,6 @@ public class CharArrayExample {
 
         System.out.print("mejtexi simvolnern en ===>> ");
         char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
-        for (int i = 0; i < chars2.length; i++) ;
         int x = chars2.length / 2;
         System.out.print(chars[x] + " | " + chars2[x - 1]);
 
@@ -33,34 +32,32 @@ public class CharArrayExample {
         boolean b = false;
         if (chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l') {
             b = true;
-        } else
-            b = false;
-        System.out.println("===>> " + b);
+            System.out.println("===>> " + b);
 
-        //4-Տպում ենք true եթե տեքստը պարունակում է bob բառը,
-        // բայց o-ի տեղը կարող է լինել ցանկացած սինվոլ։
-        System.out.println();
-        char h = 'b';
-        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
-        boolean a = false;
-        for (int i = 0; i < bobArray.length; i++) {
-            if (bobArray[i] == h && bobArray[i + 2] == h) {
-                a = true;
-                break;
-            } else
-                a = false;
-        }
-        System.out.print("===>> " + a);
-        System.out.println();
-
-        //5-Պետք է տպել մասիվի այն սինվոլները որոնք պռաբել չեն:
-
-        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
-        for (int i = 0; i < text.length; i++) {
-            if (text[i] != ' ') {
-                System.out.print(text[i]);
+            //4-Տպում ենք true եթե տեքստը պարունակում է bob բառը,
+            // բայց o-ի տեղը կարող է լինել ցանկացած սինվոլ։
+            System.out.println();
+            char h = 'b';
+            char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
+            boolean a = false;
+            for (int i = 0; i < bobArray.length; i++) {
+                if (bobArray[i] == 'b' && bobArray[i + 2] == 'b') {
+                    a = true;
+                    break;
+                }
             }
+            System.out.print("===>> " + a);
+            System.out.println();
 
+            //5-Պետք է տպել մասիվի այն սինվոլները որոնք պռաբել չեն:
+
+            char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
+            for (int i = 0; i < text.length; i++) {
+                if (text[i] != ' ') {
+                    System.out.print(text[i]);
+                }
+
+            }
         }
     }
 }
