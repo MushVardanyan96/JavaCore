eb3b23263724102cef9eaae39c449cdb21e2c44a
MushVardanyan96 <mister.musho@mail.ru>
2022-06-07 00:17:06 +0400

    Stack

---
+++ src/homework/JavaBook/chapter6/Stack.java
@@ -0,0 +1,27 @@
+package homework.JavaBook.chapter6;
+
+public class Stack {
+
+    int stck[] = new int[10];
+    int tos;
+
+    Stack() {
+        tos = -1;
+    }
+    void push(int item) {
+        if (tos == 9)
+            System.out.println("Stack zapolnen. ");
+        else
+            stck[++tos] = item;
+    }
+    int рор() {
+        if (tos < 0) {
+            System.out.println("Stack ne zagrujen. ");
+            return 0;
+        } else
+            return stck[tos--];
+
+    }
+
+
+}
